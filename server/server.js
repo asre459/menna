38ed6d7e396c3c674297eff0c2b0014ba2a930fb
asre459 <asre45900@gmail.com>
2025-08-25 11:59:59 -0300

    add the  the the client

--- server/server.js
+++ server/server.js
@@ -46,9 +46,9 @@ app.use('/api/media', require('./routes/media'));
 app.use('/api/admin', require('./routes/admin'));
 
 // --- Serve React frontend ---
-app.use(express.static(path.join(__dirname1, "/client/dist")));
+app.use(express.static(path.join(__dirname1, "/client-side/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname1, "client-side", "dist", "index.html"));
 });
 
 // --- MongoDB connection ---
